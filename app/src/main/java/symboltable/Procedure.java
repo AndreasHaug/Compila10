e5afe8a496ccffa28ddbc101e29bb9940575bdba
Andreas Haug <a-haug@hotmail.com>
2021-04-20 09:22:38 +0200

    updated the procedure argument representation

--- app/src/main/java/symboltable/Procedure.java
+++ app/src/main/java/symboltable/Procedure.java
@@ -5,12 +5,20 @@ import java.util.ArrayList;
 public class Procedure implements SymboltableInstance {
 
   String n;
-  symboltable.Var[] args;
-  Type t;
+  // symboltable.Var[] args;
+  Symboltable args;
+  symboltable.Type t;
 
-  public Procedure(String n) {
+  public Procedure(String n,
+		   Symboltable args,
+		   symboltable.Type t) {
     this.n = n;
     this.args = args;
     this.t = t;
-  }	
+  }
+
+
+  public symboltable.Type getType() {
+    return t;
+  }
 }
