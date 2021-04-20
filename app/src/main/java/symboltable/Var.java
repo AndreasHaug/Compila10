3b89bce7c231ddad01a539c73bd139a07d963eb9
Andreas Haug <a-haug@hotmail.com>
2021-04-20 09:24:02 +0200

    updated the equals method for float ensuring it can assigned to int

--- app/src/main/java/symboltable/Var.java
+++ app/src/main/java/symboltable/Var.java
@@ -4,13 +4,19 @@ import node.Exp;
 
 public class Var implements SymboltableInstance {
 
-	String n;
-	Exp e;
-	Type t;
+  private String n;
+  private Exp e;
+  private Type t;
 
-	public Var(String n, Exp e, Type t) {
-		this.n = n;
-		this.e = e;
-		this.t = t;
-	}
+  public Var(String n, Exp e, Type t) {
+    this.n = n;
+    this.e = e;
+    this.t = t;
+  }
+
+  public symboltable.Type getType() {
+    return t;
+  }
+
+  
 }
