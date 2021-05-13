71cef28047e58493c3c52adba560fbcf6d0f884a
Andreas Haug <andrea2@uio.no>
2021-05-13 09:32:28 +0200

    added codegen

--- app/src/main/java/syntaxtree/node/exp/NotExp.java
+++ app/src/main/java/syntaxtree/node/exp/NotExp.java
@@ -1,5 +1,10 @@
 package node;
 
+import bytecode.CodeFile;
+import bytecode.CodeProcedure;
+import bytecode.instructions.NOT;
+
+
 import symboltable.Symboltable;
 
 public class NotExp extends Exp {
@@ -20,9 +25,15 @@ public class NotExp extends Exp {
   @Override
   public symboltable.Type semanticAnalyze(Symboltable table) {
     if (!e.semanticAnalyze(table).equals(symboltable.Type.boolType)) {
-      throw new error.NotABool("Not expression must be on bool but is " + e.semanticAnalyze(table).toString());
+      throw new error.NotABool("Not expression must be on bool but is " +
+                               e.semanticAnalyze(table).toString());
     }
     return symboltable.Type.boolType;
   }
 
+  @Override
+  public void pushOnStack(CodeFile codefile, CodeProcedure procedure) {
+    e.pushOnStack(codefile, procedure);
+    procedure.addInstruction(new NOT());
+  }
 }
